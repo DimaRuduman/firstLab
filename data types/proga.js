732ac1c6616c7213c64ac67895b2f292f723e2f5
DimaRuduman <ruduman.dmytro_im64@edu.kpi.ua>
2026-09-14 09:29:17 +0300

    endLab (full)

--- data types/proga.js
+++ data types/proga.js
@@ -1,39 +1,22 @@
 let datas = ["hello", "my", "name", "is", "dima", true, false, true , true , false , 5, 6, 4, 4, 3, 6, 7, 8, 9, 9, null, null, null, undefined ,undefined, undefined, undefined];
+let type;
 
-let objDatas = {
-    Number: 0,
-    String: 0,
-    Boolean: 0,
-    Biglnt: 0,
-    NULL: 0,
-    undefined: 0
-};
 
-for (let data of datas) {   
-    
-if ( typeof (data) == "number"){
-    objDatas.Number++
+let objDatas = {};
+
+for (let item of datas){
+    if (item === null){
+        type = "null"
+    }    else {
+        type = typeof item;
+    }
+
+
+if (objDatas[type] == undefined){
+    objDatas[type] = 0;
 }
 
-if ( typeof (data) == "string"){
-    objDatas.String++
+objDatas[type]++
 }
 
-if ( typeof (data) == "boolean"){
-    objDatas.Boolean++
-}
-
-if ( typeof (data) == "bigint"){
-    objDatas.Biglnt++
-}   
-
-if (data == null) {
-    objDatas.NULL++
-}
-
-if ( typeof (data) == "undefined"){
-    objDatas.undefined++
-}
-}
-
-console.log (objDatas);
+console.log (objDatas)
